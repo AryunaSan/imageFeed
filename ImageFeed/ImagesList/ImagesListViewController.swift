28f27b0ba9f219f61f3847333a2a99a7f77cb11d
AryunaSan <tudupovaa@gmail.com>
2024-09-06 00:08:02 +0300

    ячейки с динамической высотой лайк проставлен у четных фотографий у фотографий стоит текущая дата края фотографий скруглены и между ними есть отступ
    задание готово к проверке

--- ImageFeed/ImagesList/ImagesListViewController.swift
+++ ImageFeed/ImagesList/ImagesListViewController.swift
@@ -5,7 +5,10 @@
 import UIKit
 
 final class ImagesListViewController: UIViewController {
+    
     @IBOutlet private var tableView: UITableView!
+    
+    private let photosName: [String] = Array(0..<20).map{ "\($0)" }
     
     private lazy var dateFormatter: DateFormatter = {
         let formatter = DateFormatter()
@@ -14,14 +17,14 @@ final class ImagesListViewController: UIViewController {
         return formatter
     }()
     
-    private let photosName: [String] = Array(0..<20).map{ "\($0)" }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        tableView.rowHeight = 200
         tableView.contentInset = UIEdgeInsets(top: 12, left: 0, bottom: 12, right: 0)
-        
+
     }
+    
 }
 
 extension ImagesListViewController: UITableViewDataSource {
@@ -30,15 +33,13 @@ extension ImagesListViewController: UITableViewDataSource {
         
     }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: ImagesListCell.reuseIdentifier, for: indexPath)
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell { let cell = tableView.dequeueReusableCell(withIdentifier: ImagesListCell.reuseIdentifier, for: indexPath)
         
         guard let imageListCell = cell as? ImagesListCell else {
             return UITableViewCell()
         }
         
         configCell(for: imageListCell, with: indexPath)
-        
         return imageListCell
     }
 }
@@ -48,24 +49,26 @@ extension ImagesListViewController {
         guard let image = UIImage(named: photosName[indexPath.row]) else {
             return
         }
-        
+
         cell.cellImage.image = image
         cell.dateLabel.text = dateFormatter.string(from: Date())
-        
+
         let isLiked = indexPath.row % 2 == 0
         let likeImage = isLiked ? UIImage(named: "likeActive") : UIImage(named: "like")
         cell.likeButton.setImage(likeImage, for: .normal)
     }
 }
 
+
 extension ImagesListViewController: UITableViewDelegate {
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {}
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) { }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         guard let image = UIImage(named: photosName[indexPath.row]) else {
             return 0
         }
-        let imageInsets = UIEdgeInsets(top: 12, left: 0, bottom: 12, right: 0)
+        
+        let imageInsets = UIEdgeInsets(top: 4, left: 16, bottom: 4, right: 16)
         let imageViewWidth = tableView.bounds.width - imageInsets.left - imageInsets.right
         let imageWidth = image.size.width
         let scale = imageViewWidth / imageWidth
