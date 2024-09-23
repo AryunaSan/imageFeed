8c54468fa2cbf415da885c53f8073933c4922641
AryunaSan <tudupovaa@gmail.com>
2024-09-23 20:01:47 +0300

    кнопка выхода не отображается на странице профиля

--- ImageFeed/Profile/ProfileViewController.swift
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -12,8 +12,8 @@ final class ProfileViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        var userpickImage = UIImage(systemName: "photo")
-        var imageView = UIImageView(image: userpickImage)
+        let userpickImage = UIImage(named: "photo")
+        let imageView = UIImageView(image: userpickImage)
         imageView.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(imageView)
         imageView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16).isActive = true
@@ -21,44 +21,47 @@ final class ProfileViewController: UIViewController {
         imageView.widthAnchor.constraint(equalToConstant: 70).isActive = true
         imageView.heightAnchor.constraint(equalToConstant: 70).isActive = true
         
-        var nameLabel = UILabel()
+        let nameLabel = UILabel()
         nameLabel.text = "Екатерина Новикова"
-        nameLabel.font = UIFont(name: "SF Pro-Bold", size: 23)
+        nameLabel.font = .boldSystemFont(ofSize: 23)
+        nameLabel.textColor = .ypWhite
         nameLabel.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(nameLabel)
         nameLabel.leadingAnchor.constraint(equalTo: imageView.leadingAnchor).isActive = true
         nameLabel.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 8).isActive = true
         self.nameLabel = nameLabel
         
-        var nicknameLabel = UILabel()
+        let nicknameLabel = UILabel()
         nicknameLabel.text = "@ekaterina_nov"
-        nicknameLabel.font = UIFont(name: "SF Pro", size: 13)
+        nicknameLabel.font = .systemFont(ofSize: 13)
+        nicknameLabel.textColor = .ypWhite
         nicknameLabel.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(nicknameLabel)
         nicknameLabel.leadingAnchor.constraint(equalTo: imageView.leadingAnchor).isActive = true
         nicknameLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 8).isActive = true
         self.nicknameLabel = nicknameLabel
         
-        var statusLabel = UILabel()
+        let statusLabel = UILabel()
         statusLabel.text = "Hello, world!"
-        statusLabel.font = UIFont(name: "SF Pro", size: 13)
+        statusLabel.font = .systemFont(ofSize: 13)
+        statusLabel.textColor = .ypWhite
         statusLabel.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(statusLabel)
         statusLabel.leadingAnchor.constraint(equalTo: imageView.leadingAnchor).isActive = true
         statusLabel.topAnchor.constraint(equalTo: nicknameLabel.bottomAnchor, constant: 8).isActive = true
         self.statusLabel = statusLabel
         
-        let button = UIButton.systemButton(
-            with: UIImage(systemName: "logOut")!,
-            target: self,
-            action: #selector(Self.didTapButton)
-        )
-        button.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(button)
-        button.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: 24).isActive = true
-        button.centerYAnchor.constraint(equalTo: imageView.centerYAnchor).isActive = true
+        let logoutButton = UIButton.systemButton(
+                    with: UIImage(named: "logOut")!,
+                    target: self,
+                    action: #selector(Self.didTapButton)
+                )
+        logoutButton.tintColor = .red
+        logoutButton.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(logoutButton)
+        logoutButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: 24).isActive = true
+        logoutButton.centerYAnchor.constraint(equalTo: imageView.centerYAnchor).isActive = true
     }
-    
     @objc
     private func didTapButton() {}
 }
