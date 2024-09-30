bab46f48b2e12c76c199d2177647bfd1d79b4193
AryunaSan <tudupovaa@gmail.com>
2024-10-01 00:24:06 +0300

    1. сверстала экран для авторизации и WebView 2. Написала запрос для получения кода авторизации, выполнила этот запрос в WebView.

---
+++ ImageFeed/Auth/AuthViewController.swift
@@ -0,0 +1,39 @@
+
+
+import UIKit
+
+final class AuthViewController: UIViewController {
+    private let webViewIdentifier = "ShowWebView"
+    
+    override func viewDidLoad() {
+        configureBackButton()
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == webViewIdentifier {
+            guard
+                let webViewViewController = segue.destination as? WebViewViewController
+            else { fatalError("Failed to prepare for \(webViewIdentifier)") }
+            webViewViewController.delegate = self
+        } else {
+            super.prepare(for: segue, sender: sender)
+        }
+    }
+    
+    private func configureBackButton() {
+        navigationController?.navigationBar.backIndicatorImage = UIImage(named: "nav_back_button")
+        navigationController?.navigationBar.backIndicatorTransitionMaskImage = UIImage(named: "nav_back_button")
+        navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
+        navigationItem.backBarButtonItem?.tintColor = .ypBlack
+    }
+}
+
+extension AuthViewController: WebViewViewControllerDelegate {
+    func webViewViewController(_ vc: WebViewViewController, didAuthenticateWithCode code: String) {
+        //TODO: process code
+    }
+
+    func webViewViewControllerDidCancel(_ vc: WebViewViewController) {
+        dismiss(animated: true)
+    }
+}
